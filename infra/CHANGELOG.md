fde1575b286b69e07a23dd22219e47a597d2c77f
Andre Landgraf <andre.timo.landgraf@gmail.com>
2024-02-23 14:47:21 -0800

    fix Terraform config

--- infra/CHANGELOG.md
+++ infra/CHANGELOG.md
@@ -7,12 +7,21 @@ Decided to try out Terraform to manage the Cloudflare infrastructure. Since this
 Initialized a new Terraform project with the following command:
 
 ```bash
+cd infra/
 terraform init
 ```
 
-Created a `main.tf` file to define the Cloudflare infrastructure for the production environment.
+Created a Terraform Cloud account. All Terraform plans and apply execute on Terraform Cloud, which eases Terraform state management. I linked Terraform Cloud as an OAuth application to my GitHub account, so that Terraform Cloud can automatically apply changes to the infrastructure when changes are pushed to the repository.
+
+Created a `main.tf` file to define the Cloudflare infrastructure for the production environment. Added the secrets to Terraform Cloud as environment variables, prefixed with `TF_VAR_`. This is done to avoid storing secrets in the repository.
 
 Next, I added my domain (aproxima.net) to Cloudflare and updated the name servers through my domain registrar (name.com) to my assigned name servers from Cloudflare. I also deleted all existing domain recordings on Cloudflare. This was done manually as I first needed the domain zone id for setting up the DNS records in Terraform.
+
+Changes can be reviewed with the following command:
+
+```bash
+terraform plan
+```
 
 ### Resources
 
--- infra/main.tf
+++ infra/main.tf
@@ -1,6 +1,6 @@
 terraform {
   cloud {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "Aproxima"
 
     workspaces {
@@ -19,6 +19,24 @@ provider "cloudflare" {
   api_token = var.CLOUDFLARE_API_TOKEN
 }
 
+variable "CLOUDFLARE_API_TOKEN" {
+  description = "API token for Cloudflare provider. Variable set on Terraform Cloud as a sensitive environment variable: TF_VAR_CLOUDFLARE_API_TOKEN."
+  type        = string
+  sensitive   = true
+}
+
+variable "CLOUDFLARE_APROXIMA_NET_ZONE_ID" {
+  description = "The zone ID for the domain aproxima.net. Variable set on Terraform Cloud as a sensitive environment variable: TF_VAR_CLOUDFLARE_APROXIMA_NET_ZONE_ID."
+  type        = string
+  sensitive   = true
+}
+
+variable "CLOUDFLARE_ACCOUNT_ID" {
+  description = "The account ID for the Cloudflare account. Variable set on Terraform Cloud as a sensitive environment variable: TF_VAR_CLOUDFLARE_ACCOUNT_ID."
+  type        = string
+  sensitive   = true
+}
+
 resource "cloudflare_worker_route" "core-api-route" {
   zone_id     = var.CLOUDFLARE_APROXIMA_NET_ZONE_ID
   pattern     = "aproxima.net/api/*"
@@ -27,7 +45,7 @@ resource "cloudflare_worker_route" "core-api-route" {
 
 resource "cloudflare_worker_script" "core-api" {
   account_id = var.CLOUDFLARE_ACCOUNT_ID
-  name = "core-api"
-  content = "console.log('Hello, world!')"
+  name       = "core-api"
+  content    = "console.log('Hello, world!')"
 }
 
