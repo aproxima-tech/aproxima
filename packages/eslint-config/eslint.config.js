620aca8ae3f3a0c00fd6bdca6f106489c9ea2d5a
Andre Landgraf <andre.timo.landgraf@gmail.com>
2024-03-01 19:29:13 -0800

    fix eslint ignores rule

--- packages/eslint-config/eslint.config.js
+++ packages/eslint-config/eslint.config.js
@@ -2,37 +2,47 @@ import eslint from '@eslint/js';
 import typescriptEslint from 'typescript-eslint';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
-export default typescriptEslint.config({
-  files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
-  ignores: ['node_modules', 'dist', 'build', 'coverage', 'public', '.wrangler', '.cache'],
-  extends: [
-    eslint.configs.recommended,
-    ...typescriptEslint.configs.recommended,
-    eslintPluginPrettierRecommended, // Keep this last so it can disable rules that conflict with Prettier, also includes eslint-config-prettier
-  ],
-  rules: {
-    'prettier/prettier': [
-      'error',
-      {
-        semi: true,
-        trailingComma: 'all',
-        singleQuote: true,
-        printWidth: 120,
-        tabWidth: 2,
-      },
-    ],
-    '@typescript-eslint/consistent-type-imports': [
-      'error',
-      {
-        prefer: 'type-imports',
-        disallowTypeAnnotations: true,
-      },
-    ],
-    // TODO: Enable these rules once ESLint flat config is supported
-    // "simple-import-sort/imports": "error",
-    // "simple-import-sort/exports": "error",
-    // "import/first": "error",
-    // "import/newline-after-import": "error",
-    // "import/no-duplicates": "error",
+export default typescriptEslint.config(
+  {
+    ignores: ['build', 'public/build', '**/build/', '**/public/build/'],
   },
-});
+  {
+    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+    linterOptions: {
+      reportUnusedDisableDirectives: 'error',
+    },
+  },
+  {
+    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+    extends: [
+      eslint.configs.recommended,
+      ...typescriptEslint.configs.recommended,
+      eslintPluginPrettierRecommended, // Keep this last so it can disable rules that conflict with Prettier, also includes eslint-config-prettier
+    ],
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          semi: true,
+          trailingComma: 'all',
+          singleQuote: true,
+          printWidth: 120,
+          tabWidth: 2,
+        },
+      ],
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {
+          prefer: 'type-imports',
+          disallowTypeAnnotations: true,
+        },
+      ],
+      // TODO: Enable these rules once ESLint flat config is supported
+      // "simple-import-sort/imports": "error",
+      // "simple-import-sort/exports": "error",
+      // "import/first": "error",
+      // "import/newline-after-import": "error",
+      // "import/no-duplicates": "error",
+    },
+  },
+);
--- packages/eslint-config/remix.js
+++ packages/eslint-config/remix.js
@@ -6,65 +6,75 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import jsxAlly from 'eslint-plugin-jsx-a11y';
 
-export default typescriptEslint.config({
-  files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
-  ignores: ['node_modules', 'dist', 'build', 'coverage', 'public', '.wrangler', '.cache'],
-  extends: [
-    eslint.configs.recommended,
-    ...typescriptEslint.configs.recommended,
-    eslintPluginPrettierRecommended, // Keep this last so it can disable rules that conflict with Prettier, also includes eslint-config-prettier
-  ],
-  plugins: {
-    react,
-    reactHooks,
-    jsxAlly,
+export default typescriptEslint.config(
+  {
+    ignores: ['build', 'public/build', '**/build/', '**/public/build/'],
   },
-  languageOptions: {
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
+  {
+    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+    linterOptions: {
+      reportUnusedDisableDirectives: 'error',
+    },
+  },
+  {
+    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+    extends: [
+      eslint.configs.recommended,
+      ...typescriptEslint.configs.recommended,
+      eslintPluginPrettierRecommended, // Keep this last so it can disable rules that conflict with Prettier, also includes eslint-config-prettier
+    ],
+    plugins: {
+      react,
+      reactHooks,
+      jsxAlly,
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
       },
     },
-    globals: {
-      ...globals.browser,
-    },
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-    formComponents: ['Form'],
-    linkComponents: [
-      { name: 'Link', linkAttribute: 'to' },
-      { name: 'NavLink', linkAttribute: 'to' },
-    ],
-    'import/resolver': {
-      typescript: {},
-    },
-  },
-  rules: {
-    'prettier/prettier': [
-      'error',
-      {
-        semi: true,
-        trailingComma: 'all',
-        singleQuote: true,
-        printWidth: 120,
-        tabWidth: 2,
+    settings: {
+      react: {
+        version: 'detect',
       },
-    ],
-    '@typescript-eslint/consistent-type-imports': [
-      'error',
-      {
-        prefer: 'type-imports',
-        disallowTypeAnnotations: true,
+      formComponents: ['Form'],
+      linkComponents: [
+        { name: 'Link', linkAttribute: 'to' },
+        { name: 'NavLink', linkAttribute: 'to' },
+      ],
+      'import/resolver': {
+        typescript: {},
       },
-    ],
-    // TODO: Enable these rules once ESLint flat config is supported
-    // "simple-import-sort/imports": "error",
-    // "simple-import-sort/exports": "error",
-    // "import/first": "error",
-    // "import/newline-after-import": "error",
-    // "import/no-duplicates": "error",
+    },
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          semi: true,
+          trailingComma: 'all',
+          singleQuote: true,
+          printWidth: 120,
+          tabWidth: 2,
+        },
+      ],
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {
+          prefer: 'type-imports',
+          disallowTypeAnnotations: true,
+        },
+      ],
+      // TODO: Enable these rules once ESLint flat config is supported
+      // "simple-import-sort/imports": "error",
+      // "simple-import-sort/exports": "error",
+      // "import/first": "error",
+      // "import/newline-after-import": "error",
+      // "import/no-duplicates": "error",
+    },
   },
-});
+);
