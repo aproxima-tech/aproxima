7444c6b2b8c16e4468601610cdcc7ad8f58f4018
Andre Landgraf <andre.timo.landgraf@gmail.com>
2024-03-16 23:56:05 -0700

    add storybook-static to eslint ignores

--- packages/ui/eslint.config.js
+++ packages/ui/eslint.config.js
@@ -1,3 +1,9 @@
 import aproximaEslintConfig from '@aproxima/eslint-config/remix';
+import { config } from 'typescript-eslint';
 
-export default aproximaEslintConfig;
+export default config(
+  {
+    ignores: ['storybook-static'],
+  },
+  ...aproximaEslintConfig,
+);
